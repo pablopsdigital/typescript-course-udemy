79a0235a0459880107b4f0cd948f29cdaefe56ec
pablopsdigital <pablopsdigital@gmail.com>
2023-08-30 12:21:57 +0100

    Section 03: numbers

---
+++ 01-init/types/numbers.ts
@@ -0,0 +1,15 @@
+(() => {
+  let avengers: number = 10;
+  console.log({ avengers });
+
+  const villians: number = 20;
+  console.log({ villians });
+
+  // Es importante tener en cuenta que aunque no asignemos
+  // tipo en typescript en javascript se evalua la expersión
+  if (avengers < villians) {
+    console.log("We are in trouble");
+  } else {
+    console.log("We are saved ");
+  }
+})();

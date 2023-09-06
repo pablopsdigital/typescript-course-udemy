1d457c3d4469ca934d717a67e9b3962e9a727772
pablopsdigital <pablopsdigital@gmail.com>
2023-09-06 14:19:27 +0100

    Section 11: Genericos

---
+++ 02-ts-node/src/generics/get-pokemon.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { Pokemon } from "../interfaces/pokemon";
+
+export const getPokemon = async (pokemonId: number): Promise<Pokemon> => {
+  console.log("Hola Mundo");
+  const res = await axios.get<Pokemon>(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
+  );
+  const { data } = res;
+  console.log(data.abilities[0].ability.name);
+
+  //if (true) throw new Error("Auxilio");
+  return data;
+};
--- 02-ts-node/src/index.ts
+++ 02-ts-node/src/index.ts
@@ -1,10 +1,25 @@
-import { Hero as SuperHero } from "./clases/hero";
+import { genericFunction, printObject } from "./generics/generics";
+import { getPokemon } from "./generics/get-pokemon";
+import { Villain } from "./interfaces/villain";
 
-console.log("Hola Mundo!");
+printObject(123);
+printObject(new Date());
+printObject({ a: 1, b: 2, c: 3 });
+printObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+printObject("Hola Mundo");
 
-const ironman = new SuperHero("Pablo", 8, 8);
+console.log(genericFunction(3.141618).toFixed(2));
+console.log(genericFunction("Hola Mundo").toLocaleLowerCase());
 
-const power = ironman.power;
+const deadtool = {
+  name: "Deadpool",
+  realName: "Wade Winston Wilson",
+  dangerLevel: 130,
+};
 
-console.log(ironman);
-console.log(power);
+console.log(genericFunction<Villain>(deadtool).dangerLevel);
+
+getPokemon(4)
+  .then((pokemon) => console.log(pokemon.sprites.front_default))
+  .catch((error) => console.log(error))
+  .finally(() => console.log("Fin "));

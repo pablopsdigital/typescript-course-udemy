6813bd4b75345ab7dbdc2e36508e06c9b5714bf1
pablopsdigital <pablopsdigital@gmail.com>
2023-09-06 11:58:50 +0100

    Section 10: Namespace en Typescript

---
+++ 02-ts-node/src/clases/hero.ts
@@ -0,0 +1,16 @@
+export class Hero {
+  constructor(
+    public name: string,
+    public powerId: number,
+    public age: number
+  ) {}
+
+  get power(): string {
+    return "yes";
+  }
+}
+
+export class Hero2 {}
+export class Hero3 {}
+export class Hero4 {}
+export class Hero5 {}
---
+++ 02-ts-node/src/index.ts
@@ -0,0 +1,10 @@
+import { Hero as SuperHero } from "./clases/hero";
+
+console.log("Hola Mundo!");
+
+const ironman = new SuperHero("Pablo", 8, 8);
+
+const power = ironman.power;
+
+console.log(ironman);
+console.log(power);
